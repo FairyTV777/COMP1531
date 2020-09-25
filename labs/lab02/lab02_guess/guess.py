318e30d801854ada4a70c7d94d380a9fd0f3b183
Yuancong <chengyuantsung@gmail.com>
2020-09-26 01:31:54 +1000

    recursion

--- labs/lab02/lab02_guess/guess.py
+++ labs/lab02/lab02_guess/guess.py
@@ -1,43 +1,15 @@
-print("Pick a number between 1 and 100 (inclusive)")
-print("My guess is: 50")
+def guess(low, high):
+    mid = (low+high) // 2
+    print("My guess is: " + str(mid))
+    print("Is my guess too low (L), too high (H), or correct (C)?")
+    answer = input()
+    if answer == 'C':
+        print("Got it!")
+    elif answer == 'H':
+        guess(low, mid)
+    elif answer == 'L':
+        guess(mid, high)
 
-higher_bound = None
-lower_bound = None
-
-print("Is my guess too low (L), too high (H), or correct (C)?")
-answer = input()
-if answer == 'L':
-    lower_bound = 50
-    higher_bound = 100
-    print("My guess is: " + str(75))
-
-    while True:
-        print("Is my guess too low (L), too high (H), or correct (C)?")
-        answer = input()
-        if answer == 'L':
-            lower_bound = (lower_bound + higher_bound) // 2
-            print("My guess is: " + str((lower_bound + higher_bound) // 2))
-        elif answer == 'H':
-            higher_bound = (lower_bound + higher_bound) // 2
-            print("My guess is: " + str((lower_bound + higher_bound) // 2))
-        elif answer == 'C':
-            break
-
-elif answer == 'H':
-    lower_bound = 1
-    higher_bound = 50
-    print("My guess is: " + str(25))
-
-    while True:
-        print("Is my guess too low (L), too high (H), or correct (C)?")
-        answer = input()
-        if answer == 'L':
-            lower_bound = (lower_bound + higher_bound) // 2
-            print("My guess is: " + str((lower_bound + higher_bound) // 2))
-        elif answer == 'H':
-            higher_bound = (lower_bound + higher_bound) // 2
-            print("My guess is: " + str((lower_bound + higher_bound) // 2))
-        elif answer == 'C':
-            break
-
-print("Got it!")
+if __name__ == '__main__':
+    print("Pick a number between 1 and 100 (inclusive)")
+    guess(1, 100)
