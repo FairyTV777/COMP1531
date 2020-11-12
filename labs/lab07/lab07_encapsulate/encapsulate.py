ee9c8a582d36bbb7acf5116ded2d1e72edcb080b
Yuancong <chengyuantsung@gmail.com>
2020-11-12 14:04:28 +1100

    Add get_name method

--- labs/lab07/lab07_encapsulate/encapsulate.py
+++ labs/lab07/lab07_encapsulate/encapsulate.py
@@ -8,7 +8,11 @@ class Student:
     def get_age(self):
         return datetime.datetime.now().year - self.birth_year
 
+    def get_name(self):
+        return self.name
+
 if __name__ == '__main__':
     s = Student("Rob", "Everest", 1961)
     years_old = s.get_age()
-    print(f"{s.name} is {years_old} old")
+    name = s.get_name()
+    print(f"{name} is {years_old} old")
